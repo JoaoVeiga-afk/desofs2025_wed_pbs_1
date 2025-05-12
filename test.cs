9930196872b9ffc87826f3b56c28351d524edb97
Luis <1240159@isep.ipp.pt>
2025-05-12 23:11:05 +0100

    issue_12: test lint

---
+++ test.cs
@@ -0,0 +1,6 @@
+﻿namespace ShopTex;
+
+                                                                                                                public class test
+                                                                                                                {
+                                                                                                                    
+                                                                                                                }
