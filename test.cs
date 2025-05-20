d069bdc10550f03ad2911c3906c9ffcaf206ed7c
Luis <1240159@isep.ipp.pt>
2025-05-20 22:29:19 +0100

    test

--- test.cs
+++ test.cs
@@ -1,4 +1,4 @@
 ﻿using System;
 namespace TestApp
 {class Program
-    {static void Main(string[] args){Console.WriteLine("ollla World!");}}}
+    {static void Main(string[] args){Console.WriteLine("olllaewgrwetgwetrgwe4gtwegtwegtrq3gt5jikopétbhqjnklºeqtbnmklçadfmnkççadsrmkn  World!");}}}
