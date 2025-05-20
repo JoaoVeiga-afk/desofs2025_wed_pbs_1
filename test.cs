162aa9016c378530b54ef6204e810faebe89b383
Luis <1240159@isep.ipp.pt>
2025-05-20 22:33:15 +0100

    test lint

--- test.cs
+++ test.cs
@@ -1,4 +1,13 @@
 ﻿using System;
 namespace TestApp
 {class Program
-    {static void Main(string[] args){Console.WriteLine("olllaewgrwetgwetrgwe4gtwegtwegtrq3gt5jikopétbhqjnklºeqtbnmklçadfmnkççadsrmkn  World!");}}}
+    {static void MaiWAWRFVBAEFVÇLMAERGFG+
+        wekrf,~Q3WÇfEV~P´
+        private KWFRMLº k;_merQDWçfAEºDLPV´+
+        LPÇ~QDWMLºEK"M: n,qd3wm.afksvlpc+ç" +
+    "b´« +vçgfd+lewrmºq32 .N,M.RKÇWFLPG+B´V N" +
+    "+GFPÇ" +
+    "TRLQA3w2m4.1n ,,e3wmkçolpf«+" +
+    "v n+´hpç" +
+    "gfltmker4.£@1NDCVCAWRGFAERFARFG<BDGFDSGn2M13KOLº(string[] args){Console.WriteLine("
+olllaewgrwetgwetrgwe4gtwegtwegtrq3gt5jikopétbhqjnklºeqtbnmklçadfmnkççadsrmkn  World!");}}}
