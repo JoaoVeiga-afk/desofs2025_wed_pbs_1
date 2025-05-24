3971c1ff48ab4da6bcbeae039801556e6d18a3ae
João Veiga <1201082@isep.ipp.pt>
2025-05-24 16:35:29 +0100

    #27-sys: Update domain to work with new DB logic

--- Domain/Users/UserEmail.cs
+++ Domain/Users/UserEmail.cs
@@ -5,11 +5,10 @@ namespace ShopTex.Domain.Users;
 public class UserEmail
 {
     [EmailAddress]
-    [CustomEmailAddress(Configs.ValidEmail)]
     public string Value { get; set; }
 
     private UserEmail() {}
-    
+
     public UserEmail(string address)
     {
         Value = address;
