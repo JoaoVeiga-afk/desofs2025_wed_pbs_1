80644ef498b7612244c9ae21bfe4694fb2b47d8a
João Veiga <1201082@isep.ipp.pt>
2025-05-31 13:51:00 +0100

    #66-feat: created tests for Authentication Service

---
+++ Tests/Services/AuthenticationServiceTest.cs
@@ -0,0 +1,80 @@
+﻿using Moq;
+using ShopTex.Domain.Shared;
+using ShopTex.Domain.Users;
+using ShopTex.Services;
+using Xunit;
+
+namespace ShopTex.Tests.Services;
+
+public class AuthenticationServiceTest
+{
+    private readonly Mock<IUnitOfWork> _unitOfWork = new Mock<IUnitOfWork>();
+    private readonly Mock<IConfiguration> _configuration = new Mock<IConfiguration>();
+    private readonly Mock<IUserRepository> _userRepository = new Mock<IUserRepository>();
+    private readonly Mock<ILogger<UserService>> _logger = new Mock<ILogger<UserService>>();
+    
+    private static readonly byte[] Salt = UserService.GeneratePasswordSalt();
+    private readonly User _testUser = new User("Testuser", "912345678", "testuser@example.com", UserService.HashString("passwd", Salt), UserRole.UserNRole.RoleName, "enabled", Salt);
+
+
+    [Fact]
+    public async Task HasPermissionTest_Pass()
+    {
+        _userRepository.Setup(repository => repository.FindByEmail(It.IsAny<string>())).ReturnsAsync(_testUser);
+
+        var service = new AuthenticationService(_unitOfWork.Object, _userRepository.Object, _configuration.Object,
+            _logger.Object);
+
+        var result = await service.hasPermission("testuser@example.com",
+            new List<UserRole> { UserRole.UserNRole, UserRole.StoreColabRole });
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task HasPermissionTest_Fail()
+    {
+        _userRepository.Setup(repository => repository.FindByEmail(It.IsAny<string>())).ReturnsAsync(_testUser);
+
+        var service = new AuthenticationService(_unitOfWork.Object, _userRepository.Object, _configuration.Object,
+            _logger.Object);
+
+        var result = await service.hasPermission("testuser@example.com",
+            new List<UserRole> { UserRole.StoreAdminRole, UserRole.SystemRole });
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task HasPermissionTest_UserNotFound()
+    {
+        _userRepository.Setup(repository => repository.FindByEmail(It.IsAny<string>())).ReturnsAsync((User)null);
+
+        var service = new AuthenticationService(_unitOfWork.Object, _userRepository.Object, _configuration.Object,
+            _logger.Object);
+
+        var result = await service.hasPermission("testuser@example.com",
+            new List<UserRole> { UserRole.UserNRole, UserRole.StoreColabRole });
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task HasPermissionTest_UserHasNoRole()
+    {
+        var testUser2 = new User("Testuser", "912345678", "testuser@example.com",
+            UserService.HashString("passwd", Salt), null, "enabled", Salt);
+
+        _userRepository.Setup(repository => repository.FindByEmail(It.IsAny<string>())).ReturnsAsync(testUser2);
+
+        var service = new AuthenticationService(_unitOfWork.Object, _userRepository.Object, _configuration.Object,
+            _logger.Object);
+
+        var result = await service.hasPermission("testuser@example.com",
+            new List<UserRole> { UserRole.UserNRole, UserRole.StoreColabRole });
+
+        Assert.False(result);
+    }
+    
+    
+}
