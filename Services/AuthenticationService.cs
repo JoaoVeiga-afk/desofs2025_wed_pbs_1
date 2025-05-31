6b37e115006b039a64c529f3d3cbbd4a8c0c479d
João Veiga <1201082@isep.ipp.pt>
2025-05-31 14:23:30 +0100

    #66-feat: created service for Authentication Service

---
+++ Services/AuthenticationService.cs
@@ -0,0 +1,32 @@
+﻿using ShopTex.Domain.Shared;
+using ShopTex.Domain.Users;
+
+namespace ShopTex.Services;
+
+public class AuthenticationService (IUnitOfWork unitOfWork, IUserRepository repo, IConfiguration config, ILogger<UserService> logger)
+{
+
+    public async Task<bool> hasPermission(string email, List<UserRole> roles)
+    {
+        var user = await repo.FindByEmail(email);
+        if (user == null)
+        {
+            logger.LogWarning("User with email {Email} not found", email);
+            return false;
+        }
+        
+        if (user.Role == null)
+        {
+            logger.LogWarning("User with email {Email} has no role assigned", email);
+            return false;
+        }
+        
+        if (roles.Contains(user.Role))
+        {
+            return true;
+        } 
+        
+        logger.LogWarning("User with email {Email} doesn't have permissions for this endpoint", email);
+        return false;
+    }
+}
