5f1c0f571acdb72e2cbbf5727d4f90edabf20c6d
João Veiga <1201082@isep.ipp.pt>
2025-05-31 22:24:02 +0100

    #66-feat: Fix issue with getting user by token endpoint and fixed code duplication

--- Configs.cs
+++ Configs.cs
@@ -13,4 +13,10 @@ public class Configs
 
     public static string DbConnection => Environment.GetEnvironmentVariable("DB_CONNECTION_SERVER") 
                                          ?? throw new Exception("DB is not set");
+    
+    public static string SYS_ADMIN_ROLE_NAME ="System Administrator";
+    public static string USER_ROLE_NAME = "Client";
+    public static string STORE_ADMIN_ROLE_NAME = "Store Administrator";
+    public static string STORE_COLAB_ROLE_NAME = "Store Collaborator";
+    
 }
